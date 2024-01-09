be629f962cabd9b54766e436a35acf97f4521de7
Chelsea Hannah <80699111+cann9676@users.noreply.github.com>
2024-01-09 14:38:20 -0500

    Setup Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
+  jxsSingleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'all'
 };
