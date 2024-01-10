cd107573b2d72fd7ad1940af6f9c1703575d6367
Chelsea Hannah <80699111+cann9676@users.noreply.github.com>
2024-01-10 10:19:14 -0500

    Checkpoint 1/10

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
   rules: {
     '@@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error'
+    '@typescript-eslint/no-explicit-any': 'error',
     'prettier/prettier': ['error', prettierOptions],
     'no-unused-vars': 2,
   }
